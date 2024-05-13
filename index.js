cfb2ca35017f3b0d80baf13580cea7f09bca01b4
Muhammad-Abrar2010 <tahseenbdserver@outlook.com>
2024-05-13 20:04:29 +0600

    updated index.js

--- index.js
+++ index.js
@@ -1,56 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const app = express();
-// const port = process.env.PORT || 5000;
-// require("dotenv").config();
-
-// //middleware
-// app.use(cors());
-// app.use(express.json());
-
-// app.get("/", (req, res) => {
-//   res.send("Blog Hub Server Running");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Blog Hub Server Running in ${port}`);
-// });
-
-// //mongodb
-
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ubfbj4e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-
-// async function run() {
-//   try {
-//     await client.connect();
-
-//     const blogCollection = client.db("blogDB").collection("blog");
-
-//     app.post("/blogs", async (req, res) => {
-//       const newBlog = req.body;
-//       console.log(newBlog);
-//       const result = await blogCollection.insertOne(newBlog);
-//       // res.send(result);
-//     });
-//     await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   } finally {
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -89,6 +36,12 @@ async function startServer() {
       const newBlog = req.body;
       console.log(newBlog);
       const result = await blogCollection.insertOne(newBlog);
+      res.send(result);
+    });
+
+    app.get("/blogs", async (req, res) => {
+      const cursor = blogCollection.find();
+      const result = await cursor.toArray();
       res.send(result);
     });
 
